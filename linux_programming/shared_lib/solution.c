179bf9cee3377b80293baf3ca0514c1588f00a8a
hyperb0rean <jewellin02@gmail.com>
2024-06-03 18:39:35 +0300

    init

---
+++ linux_programming/shared_lib/solution.c
@@ -0,0 +1,20 @@
+#include "stddef.h"
+#include "stdio.h"
+#include "dlfcn.h"
+#include "stdlib.h"
+
+
+
+int (*pimpl(const char* libname, const char* funcname))(int) {
+    void* handle = dlopen(libname, RTLD_LAZY);
+    if(NULL == handle) {
+        return NULL;
+    } else {
+        return (int(*)(int)) dlsym(handle, funcname);
+    }
+}
+
+int main(int argc, char** argv) {
+    printf("%d\n",pimpl(argv[1], argv[2])(atoi(argv[3])));
+    return 0;
+}
